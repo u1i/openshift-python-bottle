631c884eaf73946ac197ca2900d179b84853f99e
Uli <uli.hitzel@gmail.com>
2018-08-06 15:24:54 +0800

    changed

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,14 @@
-from bottle import Bottle
+from bottle import Bottle, response
 application = Bottle(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+	return "Hello from Bottle!"
+
+@application.route("/api")
+def hello():
+	response.content_type = "application/json"
+	return '{"message":"Hello from Bottle!"}'
 
 if __name__ == "__main__":
-    application.run()
+	application.run()
