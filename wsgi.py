84b7c13c43d35d43b94fc7874352baff873a6098
Uli <uli.hitzel@gmail.com>
2018-08-06 15:58:32 +0800

    changed

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,17 @@
 from bottle import Bottle, response
+from datetime import datetime
+
 application = Bottle(__name__)
 
 @application.route("/")
 def hello():
-	return "Hello from Bottle!"
+	return "Hello from Bottle!<hr>" + str(datetime.now())
 
 @application.route("/api")
 def hello():
 	response.content_type = "application/json"
-	return '{"message":"Hello from Bottle!"}'
+	stuff = { "message":"Hello from Bottle!","time": str(datetime.now())}
+	return dict(stuff)
 
 if __name__ == "__main__":
 	application.run()
